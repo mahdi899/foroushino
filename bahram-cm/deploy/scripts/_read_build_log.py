83378b28c7d5bd232ccde985d52e4eb5b873613f
Ehsan Ghodrati <68652506+Ehsanghodratiofficial@users.noreply.github.com>
2026-07-22 00:49:57 +0330

    debug final1

--- bahram-cm/deploy/scripts/_read_build_log.py
+++
@@ -1,6 +0,0 @@
-from _deploy_common import configure_stdout, connect, load_deploy_env
-configure_stdout()
-c = connect(load_deploy_env(), timeout=60)
-_, out, _ = c.exec_command("cat /tmp/bahram-frontend-build.log", timeout=30)
-print(out.read().decode("utf-8", "replace")[-8000:])
-c.close()
